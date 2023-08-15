c5d42de5bb10b5974e8ca79eb2f51e70689bc566
Connor Ashton <cjconnorashton@gmail.com>
2023-08-14 20:19:17 -0600

    more cleanup

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	let messageFeed: string[] = [''];
-	let myMessage: string = '';
-	let channel_input_val: string = 'global';
+	let message: string = '';
+	let channelInputVal: string = 'global';
 	let currentChannel: string = 'global';
 
 	let name: string = '';
@@ -20,7 +20,7 @@
 
 	const changeWS = () => {
 		ws.close();
-		currentChannel = channel_input_val;
+		currentChannel = channelInputVal;
 		messageFeed = [];
 		setupWS();
 	};
@@ -30,8 +30,8 @@
 			alert('Please enter your name');
 			return;
 		}
-		ws.send(name + ': ' + myMessage);
-		myMessage = '';
+		ws.send(name + ': ' + message);
+		message = '';
 	};
 </script>
 
@@ -40,14 +40,14 @@
 <p>Listening on channel: {currentChannel}</p>
 
 <label for="channel">Channel:</label>
-<input id="channel" placeholder="write channel here" bind:value={channel_input_val} />
+<input id="channel" placeholder="write channel here" bind:value={channelInputVal} />
 <button on:click={changeWS}>Switch</button>
 
 <label for="name">Name:</label>
 <input id="name" placeholder="write name here" bind:value={name} />
 
 <label for="message">Message:</label>
-<input id="message" placeholder="write message here" bind:value={myMessage} />
+<input id="message" placeholder="write message here" bind:value={message} />
 <button on:click={sendMessage}>Send</button>
 
 {#each messageFeed as message}
