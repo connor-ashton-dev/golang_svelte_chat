2937570e17aeec7064fa54a61a4082d511d5a878
Connor Ashton <cjconnorashton@gmail.com>
2023-08-15 15:35:01 -0600

    see who is on the page

--- client/src/routes/chat/+page.svelte
+++ client/src/routes/chat/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { name } from '$lib/store';
+	import { onDestroy, onMount } from 'svelte';
+	import { name, room } from '$lib/store';
+	import { v4 as uuidv4 } from 'uuid';
 	let messageFeed: string[] = [''];
+	let currentUsers: string[] = [''];
 	let message: string = '';
 	let channelInputVal: string = '';
-	let currentChannel: string = 'global';
+	let currentChannel = $room;
+	let myId: string = $name + '::' + uuidv4();
 
 	let ws: WebSocket;
 	onMount(() => {
@@ -17,8 +20,24 @@
 
 	const setupWS = () => {
 		ws = new WebSocket('wss://server-xcjt64gdjq-wm.a.run.app/ws/' + currentChannel);
+		// ws = new WebSocket('ws://localhost:3000/ws/' + currentChannel);
 		ws.onmessage = (event) => {
-			messageFeed = [...messageFeed, event.data];
+			const data = event.data;
+			if (data.includes('users::')) {
+				const userArray = data.split('::');
+				console.log(userArray);
+				currentUsers = userArray.slice(1, userArray.length);
+			}
+			if (data.includes('message::')) {
+				const user = data.split('::')[1];
+				const message = data.split('::')[2];
+				messageFeed = [...messageFeed, user + ': ' + message];
+			}
+		};
+		ws.onopen = () => {
+			if ($name != '') {
+				ws.send('join::' + myId);
+			}
 		};
 	};
 
@@ -30,7 +49,7 @@
 	};
 
 	const sendMessage = () => {
-		ws.send($name + ': ' + message);
+		ws.send('message::' + $name + '::' + message);
 		message = '';
 	};
 </script>
@@ -47,16 +66,20 @@
 			/>
 			<button class="text-white bg-blue-500 p-2 rounded-r-lg" on:click={changeWS}>Change</button>
 		</div>
+		<p>Current Users:</p>
+		<ul class="text-green-500">
+			{#each currentUsers as user}
+				<li>{user}</li>
+			{/each}
+		</ul>
 	</div>
 
-	<div class="w-full flex flex-col justify-center items-center mt-20">
-		<p class="text-4xl">Convo:</p>
-		<div class="py-4">
-			{#each messageFeed as message}
-				<p>{message}</p>
-			{/each}
-		</div>
-		<div class="w-full flex flex-row justify-center lg:mb-60 sm:mb-10">
+	<div class=" flex flex-col justify-center items-center mt-20">
+		<p class="text-4xl mb-10">Convo:</p>
+		{#each messageFeed as message}
+			<p>{message}</p>
+		{/each}
+		<div class="w-full flex flex-row justify-center mt-10 lg:mb-60 sm:mb-10">
 			<input
 				class="border border-black outline-none p-1 rounded-l-lg"
 				placeholder="Send Message"
--- server/socket.go
+++ server/socket.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"strings"
 	"sync"
 	"time"
 
@@ -23,31 +24,126 @@ func NewServer() *Server {
 	}
 }
 
+// readLoop reads messages from the client and broadcasts them to all other clients
+func (s *Server) readLoop(ws *websocket.Conn, channel string) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("error reading from client: ", err)
+			continue
+		}
+		msg := buf[:n]
+		stringMsg := string(msg)
+		parts := strings.Split(stringMsg, "::")
+
+		if len(parts) < 3 {
+			fmt.Println("error: invalid message format")
+			continue
+		}
+
+		if parts[0] == "message" { // MESSAGE
+			s.cleanInactiveSockets(channel)
+			s.broadcast([]byte(msg), channel)
+			s.sendUsers(ws, channel)
+		}
+
+		if parts[0] == "join" { // JOIN
+			username := parts[1] + "::" + parts[2]
+			s.cleanInactiveSockets(channel)
+			s.addUser(ws, channel, username)
+			s.sendUsers(ws, channel)
+		}
+	}
+}
+
+func (s *Server) addUser(ws *websocket.Conn, channel, username string) {
+	// add user to channel
+	s.channels[channel].mut.Lock()
+	s.channels[channel].conns[ws] = &Socket{
+		id: "foo",
+	}
+	s.channels[channel].mut.Unlock()
+
+	// add user to users
+	s.channels[channel].mut.Lock()
+	s.channels[channel].users[ws] = username
+	s.channels[channel].mut.Unlock()
+
+	fmt.Println(s.channels[channel].users)
+}
+
+func (s *Server) sendUsers(ws *websocket.Conn, channel string) {
+	//loop thru users
+	usersString := ""
+	for _, user := range s.channels[channel].users {
+		username := strings.Split(user, "::")[0]
+		usersString += "::" + username
+
+	}
+	s.broadcast([]byte("users"+usersString), channel)
+}
+
+// loop thru all sockets in channel and clean inactive ones
+func (s *Server) cleanInactiveSockets(channel string) {
+	for ws := range s.channels[channel].conns {
+		if _, err := ws.Write([]byte("")); err != nil {
+			if _, ok := s.channels[channel].conns[ws]; ok {
+				fmt.Println("in channel but not avaliable. deleting...")
+				s.channels[channel].mut.Lock()
+				delete(s.channels[channel].conns, ws)
+				delete(s.channels[channel].users, ws)
+				s.channels[channel].mut.Unlock()
+			} else {
+				fmt.Println("error at cleanInactiveSockets: ", err)
+			}
+		}
+	}
+}
+
+// broadcast sends a message to all clients in a channel
+func (s *Server) broadcast(b []byte, channel string) {
+	for ws := range s.channels[channel].conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				// If there's an error while writing, delete the socket from the map
+				s.channels[channel].mut.Lock()
+				delete(s.channels[channel].conns, ws)
+				delete(s.channels[channel].users, ws)
+				s.channels[channel].mut.Unlock()
+			}
+		}(ws)
+	}
+}
+
 // ------------------  CHANNEL  ------------------
 
 type Channel struct {
 	mut   sync.Mutex
 	conns map[*websocket.Conn]*Socket
+	users map[*websocket.Conn]string
 }
 
 func newChannel() *Channel {
 	return &Channel{
 		conns: map[*websocket.Conn]*Socket{},
 		mut:   sync.Mutex{},
+		users: map[*websocket.Conn]string{},
 	}
 }
 
 // ------------------  SOCKET  ------------------
 
 type Socket struct {
-	id        string
-	connected bool
+	id string
 }
 
 func (s *Socket) newSocket(id string) *Socket {
 	return &Socket{
-		id:        id,
-		connected: true,
+		id: id,
 	}
 
 }
@@ -67,49 +163,13 @@ func (s *Server) HandleWS(ws *websocket.Conn) {
 		fmt.Printf("channel %s does not exist. creating...\n", channel)
 		s.channels[channel] = newChannel()
 	}
+
 	s.channels[channel].mut.Lock()
 	s.channels[channel].conns[ws] = &Socket{
-		id:        "foo",
-		connected: true,
+		id: "foo",
 	}
 	s.channels[channel].mut.Unlock()
 	s.readLoop(ws, channel)
-}
-
-// readLoop reads messages from the client and broadcasts them to all other clients
-func (s *Server) readLoop(ws *websocket.Conn, channel string) {
-	buf := make([]byte, 1024)
-	for {
-		n, err := ws.Read(buf)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Println("error reading from client: ", err)
-			continue
-		}
-		msg := buf[:n]
-
-		s.broadcast(msg, channel)
-	}
-}
-
-// broadcast sends a message to all clients in a channel
-func (s *Server) broadcast(b []byte, channel string) {
-	for ws := range s.channels[channel].conns {
-		go func(ws *websocket.Conn) {
-			if _, err := ws.Write(b); err != nil {
-				if _, ok := s.channels[channel].conns[ws]; ok {
-					fmt.Println("in channel but not avaliable. deleting...")
-					s.channels[channel].mut.Lock()
-					delete(s.channels[channel].conns, ws)
-					s.channels[channel].mut.Unlock()
-				} else {
-					fmt.Println("error writing to client: ", err)
-				}
-			}
-		}(ws)
-	}
 }
 
 // ------------------  FEED  ------------------
